2659be9a3733633feb811dfa8487d5bff329efc3
Sag <saghir786@gmail.com>
2016-12-22 21:21:47 +0000

    changes

--- OLED_FUEL/OLED_FUEL.ino
+++ OLED_FUEL/OLED_FUEL.ino
@@ -15,52 +15,67 @@ Adafruit_SSD1306 Display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 #define YPOS 1
 #define DELTAY 2
 
+int A0PIN = 0;
+
 #if (SSD1306_LCDHEIGHT != 64)
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
+void DrawBar(int reading);
+
 void setup()   {                
   Serial.begin(9600);
-
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   Display.begin(SSD1306_SWITCHCAPVCC);
-  // init done
   Display.clearDisplay();
-
-  DrawTitles();
-  
-  Display.drawRect(2,30,120,34,WHITE);
-
-  DrawBar(300);
-  //Display.drawPixel(35, 60, WHITE);
-
+  DrawTitles(false);
+  //DrawBar(readPotValue());
   Display.display();
 }
 
-
 void loop() {
-
+   //Display.clearDisplay();
+   DrawBar(readPotValue());
+   delay(5000);
+   Display.clearDisplay();
+   //doDraw(512);
+   //delay(5000);
 }
 
-void DrawTitles(void) {
+int readPotValue(){
+  return analogRead(A0PIN);  
+}
 
+void DrawTitles(boolean fuelLow) {
   Display.setTextSize(3);
   Display.setTextColor(WHITE);
   Display.setCursor(0, 0);
-  Display.println("FUEL");
-
+  //if (fuelLow) {
+  //  Display.println("FUEL LOW");
+  //} else {
+    Display.println("FUEL");
+  //}
+  Display.display();
 }
 
 void DrawBar(int inputReading){
-  double readingAsPercentage = ((double)inputReading/512);
+  Display.drawRect(2,30,120,34,WHITE);
+  Display.display();
+  double readingAsPercentage = ((double)inputReading/388);
   double barWidth = 118*readingAsPercentage;
   Serial.print("Reading:");
+  Serial.print(inputReading);
   printDouble(readingAsPercentage, 10);
   Serial.print(" Bar:");
-  printDouble(barWidth, 10);    
-  Display.fillRect(4,33,barWidth+2,28,WHITE);  
+  printDouble(barWidth, 10);
 
-  if (barWidth <105){
+  Display.fillRect(4,33,118,28,BLACK);      
+  Display.display();
+  Display.fillRect(4,33,barWidth+2,28,WHITE);  
+  Display.display();
+  
+  if (barWidth < 105 && barWidth > 10){
     Display.fillRect(barWidth+4,38,6,18,WHITE);
+    Display.display();
   }
 }
 
